2a084f2da45941c3a9583ec4de8677933840f503
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2024-08-17 14:07:06 +0530

    commit

---
+++ frontEnd/my-project/src/components/Freebook.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import list from '../../public/list.json'
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick"
+import Card from './Card';
+
+const Freebook = () => {
+
+    const items=[1,2,3,4,5];
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+
+    const filteredData= list.filter((data)=>data.category==="free");
+
+  return (
+    <>
+        <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
+            <div>
+            <h1 className='font-semibold text-2xl pb-2'>Free Offered Courses</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo esse quae iusto magni perferendis aut quia, a
+                liquam itaque aliquid. Repellat aspernatur, 
+                molestias reprehenderit consectetur harum molestiae quae! Dolores, cum tenetur. </p>
+            </div>
+
+        {/* Slider */}
+        <div>
+            <Slider {...settings}>
+                {
+                    items.map((item)=>{
+                        console.log(item);
+                        <Card key={item} item={item}/>
+                    })
+                }
+                {/* <Card/> */}
+
+            </Slider>
+            </div>
+    </div>
+    </>
+  )
+}
+
+export default Freebook
